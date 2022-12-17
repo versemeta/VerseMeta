ae6cf92a0da91654a521600fc01d4424d68d3fc2
versemeta <120814743+versemeta@users.noreply.github.com>
2022-12-17 18:17:01 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to VerseMeta!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by Join <code>@VerseMetaBsc</code>
         </p>
       </main>
 
